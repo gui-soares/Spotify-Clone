f48ad75ad5296b149ff6083f81c04e214307af2a
gui-soares <eng.guilhermemsoares@gmail.com>
2019-07-08 15:12:53 -0300

    Duck de playlist

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 
+import playlits from './playlists';
+
 const reducers = combineReducers({
-  // Remova essa linha depois de adicionar seus ducks
-  example: () => [],
+  playlits,
 });
 
 export default reducers;
