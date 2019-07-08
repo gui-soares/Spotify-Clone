46d5bef4113d1bd04c03c8424b1429c365570229
gui-soares <eng.guilhermemsoares@gmail.com>
2019-07-08 14:58:19 -0300

    Configurando Redux & Saga

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import store from './store';
 
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
@@ -11,19 +15,21 @@ import Header from './components/Header';
 import { Wrapper, Container, Content } from './styles/components';
 
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <GlobalStyle />
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <GlobalStyle />
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
